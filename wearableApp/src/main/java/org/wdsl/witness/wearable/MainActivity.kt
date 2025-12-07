cc95054e188582ccb065ff735ad882bc9a354dd6
Leso246 <simolesi02@gmail.com>
2025-12-07 16:20:14 +0100

    Hotfix

--- wearableApp/src/main/java/org/wdsl/witness/wearable/MainActivity.kt
+++ wearableApp/src/main/java/org/wdsl/witness/wearable/MainActivity.kt
@@ -1,8 +1,6 @@
 package org.wdsl.witness.wearable
 
 import android.content.Context
-import android.graphics.drawable.shapes.OvalShape
-import android.media.TimedText
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -12,7 +10,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PowerSettingsNew
 import androidx.compose.material3.Icon
@@ -26,16 +23,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.fromColorLong
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material3.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
 import org.wdsl.witness.wearable.theme.WitnessTheme
 import com.google.android.gms.wearable.Wearable
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -44,13 +40,13 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp()
         }
     }
 }
 
 @Composable
-fun WearApp(context: String) {
+fun WearApp() {
     val context = LocalContext.current
     WitnessTheme {
         Box(
@@ -68,29 +64,7 @@ fun WearApp(context: String) {
 @Composable
 fun SendHelp(context: Context) {
     var lastTapTime by remember { mutableLongStateOf(0L) }
-//    Button(
-//        onClick = {
-//            // Double tap detection
-//            val currentTime = System.currentTimeMillis()
-//            if(currentTime - lastTapTime < 400L) {
-//                sendMessageToPhone("/WitnessHelpMessage", context)
-//            }
-//            lastTapTime = currentTime
-//        },
-//        Modifier
-//            .size(140.dp)
-//            .background(MaterialTheme.colors.primary)
-//            .clip(CircleShape),
-//        shape = CircleShape
-//    )
-//    {
-//        Text(
-//            stringResource(R.string.send_help),
-//            fontWeight = FontWeight.Bold,
-//            textAlign = TextAlign.Center,
-//            fontSize = 24.sp
-//        )
-//    }
+
     IconButton (
         modifier = Modifier
             .fillMaxSize(0.7f)
@@ -139,5 +113,5 @@ private fun sendMessageToPhone(path: String, context: Context) {
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
