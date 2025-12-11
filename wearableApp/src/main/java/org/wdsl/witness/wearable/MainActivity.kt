02cf84b92284e7dd94e678db0045b1a097413d15
Leso246 <simolesi02@gmail.com>
2025-12-11 22:11:26 +0100

    Code refactor in MainActivity.kt and HelpButton.kt

--- wearableApp/src/main/java/org/wdsl/witness/wearable/MainActivity.kt
+++ wearableApp/src/main/java/org/wdsl/witness/wearable/MainActivity.kt
@@ -1,23 +1,17 @@
 package org.wdsl.witness.wearable
 
+import android.R.style.Theme_DeviceDefault
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.EmergencyRecording
-import androidx.compose.material.icons.filled.Sports
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,17 +23,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material3.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.delay
 import org.wdsl.witness.shared.WearableMessageConstants
+import org.wdsl.witness.wearable.composables.HelpButton
 import org.wdsl.witness.wearable.theme.WitnessTheme
 import org.wdsl.witness.wearable.util.ConfirmationMessageState
 
@@ -50,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        setTheme(android.R.style.Theme_DeviceDefault)
+        setTheme(Theme_DeviceDefault)
 
         setContent {
             WearApp()
@@ -69,29 +62,26 @@ fun WearApp() {
             contentAlignment = Alignment.Center
         ) {
             TimeText()
-            SendHelp(context)
+            SendHelpScreen(context)
         }
     }
 }
 
 @Composable
-fun SendHelp(context: Context) {
+fun SendHelpScreen(context: Context) {
     var lastTapTime by remember { mutableLongStateOf(0L) }
     var isPressed by remember { mutableStateOf(false) }
-    var whistleIcon by remember { mutableStateOf(false) }
+    var whistleLongPress by remember { mutableStateOf(false) }
     val isConfirmed by ConfirmationMessageState.isConfirmed.collectAsState()
 
     val progress = remember { Animatable(0f) }
 
-    val animatedColor by animateColorAsState(
-        targetValue = if (isPressed) Color(0xFF008B8B) else Color(0xFF3DCFDC),
-        label = "Button Color Animation"
-    )
-
+    // When the button is being hold, wait 200ms and then change the icon and start the
+    // animation of the progress bar
     LaunchedEffect(isPressed) {
         if (isPressed) {
             delay(200L)
-            whistleIcon = true;
+            whistleLongPress = true;
             progress.snapTo(0f)
             progress.animateTo(
                 targetValue = 1f,
@@ -102,53 +92,31 @@ fun SendHelp(context: Context) {
         }
     }
 
-    Box(contentAlignment = Alignment.Center) {
-        if (whistleIcon) {
-            /***
-             * https://kotlinlang.org/api/compose-multiplatform/material3/androidx.compose.material3/-circular-progress-indicator.html
-             * https://developer.android.com/reference/com/google/android/material/progressindicator/CircularProgressIndicator
-             */
-            CircularProgressIndicator(
-                modifier = Modifier.fillMaxSize(0.8f),
-                progress = { progress.value },
-                color = MaterialTheme.colorScheme.secondary,
-                trackColor = MaterialTheme.colorScheme.surfaceVariant,
-                strokeWidth = 12.dp
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxSize(0.7f)
-                .background(color = animatedColor, shape = CircleShape)
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onPress = {
-                            isPressed = true
-                            tryAwaitRelease() // Wait until the finger is lifted
-                            whistleIcon = false;
-                            isPressed = false
-                        },
-                        onDoubleTap = {
-                            Log.d("WearMessageService", "Double Tap Detected")
-                            sendMessageToPhone(WearableMessageConstants.HELP_MESSAGE_PATH, context)
-                        },
-                    )
+    HelpButton(
+        isConfirmed = isConfirmed,
+        whistleLongPress = whistleLongPress,
+        isPressed = isPressed,
+        progress = progress.value,
+        modifier = Modifier.pointerInput(Unit) {
+            detectTapGestures(
+                onPress = {
+                    isPressed = true
+                    tryAwaitRelease() // Wait until the finger is lifted
+                    whistleLongPress = false;
+                    isPressed = false
                 },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                modifier = Modifier.fillMaxSize(0.8f),
-                imageVector = if(!whistleIcon) Icons.Filled.EmergencyRecording else Icons.Filled.Sports,
-                contentDescription = "Emergency Button",
-                tint = if(isConfirmed && !whistleIcon) Color.Red else Color.Black
+                onDoubleTap = {
+                    Log.d("WearMessageService", "Double Tap Detected")
+                    sendMessageToPhone(WearableMessageConstants.HELP_MESSAGE_PATH, context)
+                },
             )
-        }
-    }
+        },
+    )
 }
 
-// Generic fun to send a message to the phone
-private fun sendMessageToPhone(path: String, context: Context) {
+
+// Generic fun to send a message to the phone with a specific path
+fun sendMessageToPhone(path: String, context: Context) {
     val messageClient = Wearable.getMessageClient(context)
 
     Wearable.getNodeClient(context).connectedNodes
